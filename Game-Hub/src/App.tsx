e2e2009a776eb660b19e22ca12fedfdbfeb1e086
Walipieta <kacperlatarski418@gmail.com>
2024-03-12 14:55:07 +0100

    nav Bar building

--- Game-Hub/src/App.tsx
+++ Game-Hub/src/App.tsx
@@ -1,11 +1,29 @@
-import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <Button colorScheme="blue">Jebac psy</Button>
-    </div>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
