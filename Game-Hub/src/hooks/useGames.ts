f1a1951c1fe6630c1f0c5e4118bdb91dd62cd92a
Walipieta <kacperlatarski418@gmail.com>
2024-06-21 21:31:42 +0200

    Get optimized Images

--- Game-Hub/src/hooks/useGames.ts
+++ Game-Hub/src/hooks/useGames.ts
@@ -29,22 +29,23 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
-  
+    
     useEffect(() => {
      const controller = new AbortController();
         
-     
+        
         apiClient
          .get<FetchGamesResponse>("/games",{ signal: controller.signal})
          .then((res) => setGames(res.data.results))
          .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err.message)});
+            setError(err.message)
+        });
         return () => controller.abort();
     },  []);
 
 
-    return {games ,error };
+    return {games ,error  };
   
 }
 
